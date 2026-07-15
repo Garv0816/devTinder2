771c47f1d6da90153da48d65e49ad4e2503e2142
Garv0816 <garv020596@gmail.com>
2026-07-15 15:51:33 +0530

    adding validation

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@ const connectDB = require("./config/database")
 const User = require("./models/users")
 const app = express()
 
+app.use(express.json())
+// sign up api 
 app.post("/signup" , async(req ,res)=>{
-    const user = new User({
-        firstName : "Gunjan",
-        lastName : "Kumari",
-        email : "gunjan@gmail.com",
-        password : "123"
-    })
+    console.log(req.body)
+    const user = new User(req.body)
 try{
  await user.save()
     res.send("User Added Successfully")
@@ -20,6 +18,37 @@ catch(err){
    
 }
 )
+
+// get user data by email
+app.get("/user", async (req, res) => {
+    const users = await User.find({email:req.body.email });
+    if(users.length ===0){res.status(401).send("User Not Found")}
+    else{
+    res.send(users);
+    }
+   
+});
+
+// get all user data in database
+app.get("/feed", async (req ,res)=>{
+    const allUsers = await User.find({})
+    res.send(allUsers)
+})
+
+//Updating the user data
+app.patch("/user",async (req,res) => {  
+    const userID =  req.body.userID
+    const data =  req.body
+    await User.findByIdAndUpdate(userID,data , {runValidators : true})
+    res.send("User Updated Successfully")
+    
+})
+//delete API
+app.delete("/delete" , async(req ,res)=>{
+    const userID =  req.body.userID
+    await User.findByIdAndDelete(userID)
+    res.send("User Deleted Successfully")
+})
 
 // connecting to our DATA BASE OF MongoDB
 connectDB().then(
--- src/models/users.js
+++ src/models/users.js
@@ -1,23 +1,50 @@
 const mongoose = require("mongoose")
 const userSchema = mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minlength : 3,
+        maxlenght : 50
+        
     },
     lastName : {
         type : String
     },
     email : {
-        type : String
+        type : String,
+        required : true,
+        unique: true,
+        trim :true
     },
     password : {
         type : String
     },
     age : {
-        type : Number
+        type : Number,
+        min : 18
     },
     gender : {
-        type : String
+        type : String,
+        validate(value){
+            if(!["male" , "female" , "other"].includes(value)){
+                throw new Error("Invalid Gender")
+            }
+        }
+       
+    },
+    PhotoURL : {
+        type: String,
+        default : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNWr0iIiYwfxb1MpyodoY45N8QTPFp-sphaeHtF7gnrA&s=10"
+    },
+    skills : {
+        type : [String]
+        
+    },
+    about:{
+        type : String,
+        default : "Hi there ! "
     }
+
 })
 
 const User = mongoose.model("User" , userSchema)
